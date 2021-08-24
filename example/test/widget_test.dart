dad4d95af9f7c3c9722b3b19e985dc5246193fbe
volskaya <38878482+volskaya@users.noreply.github.com>
2021-08-24 14:49:24 +0300

    Delete example directory

--- example/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:lottie_example/main_app.dart';
-
-void main() {
-  testWidgets('Main sample', (tester) async {
-    await tester.pumpWidget(const App());
-    await tester.pump();
-  });
-}
